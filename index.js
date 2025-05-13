d4992bce48d1cba6673c66d3e9d61efaa06f2e1f
Michael-Khoury <mkhoury@mun.ca>
2025-05-13 13:21:41 -0230

    data base implemented

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import nodemailer from 'nodemailer';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
 
 dotenv.config(); // loads .env file
 
@@ -12,11 +14,20 @@ const PORT = process.env.PORT || 5000;
 app.use(cors()); // allows frontend to talk to this server
 app.use(express.json()); // parses incoming JSON
 
+const Message = mongoose.model('Message', {
+  name: String,
+  email: String,
+  message: String,
+  date: { type: Date, default: Date.now }
+});
+
 // Route to handle contact form submission
 app.post('/api/contact', async (req, res) => {
   const { name, email, message } = req.body;
 
   try {
+
+    await new Message({ name, email, message }).save();
     // create transporter
     const transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -25,6 +36,9 @@ app.post('/api/contact', async (req, res) => {
         pass: process.env.EMAIL_PASS,       // Gmail App Password
       },
     });
+
+    
+
 
     // send email
     await transporter.sendMail({
@@ -40,6 +54,11 @@ app.post('/api/contact', async (req, res) => {
     res.status(500).json({ message: 'Failed to send email.' });
   }
 });
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ Connected to MongoDB'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
+
 
 // Start the server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
